855c9f9f7099991c54f3aca72af95e74c36ef091
toxx <txx1337@proton.me>
2026-09-19 22:30:19 +0200

    add borgbase vaultwarden backup

--- containers/vaultwarden.nix
+++ containers/vaultwarden.nix
@@ -1,11 +1,15 @@
 {
+  pkgs,
   config,
   containerName,
   containerDomain,
   containerPort,
   ...
 }:
-
+let
+  vaultwarden-path = "/var/lib/vaultwarden";
+  backup-file = "${vaultwarden-path}/backups/db-latest.sqlite3";
+in
 {
   config = {
     networking.hostName = containerName;
@@ -34,7 +38,11 @@
           ADMIN_TOKEN=${config.sops.placeholder."vault-admin-token"}
         '';
       };
+      secrets.borgbase-ssh-key = { };
+      secrets.borgbase-enc-key = { };
     };
+
+    environment.systemPackages = [ pkgs.sqlite ];
 
     services.vaultwarden = {
       enable = true;
@@ -54,5 +62,39 @@
       requires = [ "sops-install-secrets.service" ];
     };
 
+    programs.ssh.knownHosts."ok3apsyr.repo.borgbase.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMS3185JdDy7ffnr0nLWqVy8FaAQeVh1QYUSiNpW5ESq";
+
+    services.borgbackup.jobs.borgbase = {
+      repo = "ssh://ok3apsyr@ok3apsyr.repo.borgbase.com/./repo";
+      paths = [
+        vaultwarden-path
+      ];
+      exclude = [
+        "*/db.sqlite3*"
+        "*/tmp"
+        "*/icon_cache"
+      ];
+      preHook = ''
+        mkdir -p ${vaultwarden-path}/backups
+        rm -f ${backup-file}
+        sqlite3 ${vaultwarden-path}/db.sqlite3 \
+        "VACUUM INTO '${backup-file}'"
+      '';
+      postHook = "rm -f ${backup-file}";
+      readWritePaths = [ vaultwarden-path ];
+      encryption.mode = "repokey-blake2";
+      passCommand = "cat ${config.sops.secrets.borgbase-enc-key.path}";
+      environment.BORG_RSH = "ssh -i ${config.sops.secrets.borgbase-ssh-key.path}";
+      compression = "auto,zstd";
+      prune.keep = {
+        daily = 7;
+        weekly = 4;
+        monthly = 6;
+      };
+      startAt = "daily";
+      persistentTimer = true;
+      doInit = true;
+    };
   };
 }
